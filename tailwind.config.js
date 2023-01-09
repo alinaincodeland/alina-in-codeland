5c83f97799102ad66438b39c33b2dded7baaa606
alinaincodeland <alinacuznetov@gmail.com>
2023-01-09 09:34:09 +0100

    added first version

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./pages/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
